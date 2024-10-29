8e06ebd7567504b56cd9ca7f83294effee9a165b
Erick Gathinji <ericgathinji@gmail.com>
2024-10-30 00:27:10 +0300

    If stmts

--- mosh.py
+++ mosh.py
@@ -24,5 +24,45 @@
 # print(course[0])
 # print(course[0:3]) #doesn't return 3rd character
 # print(course[:])
+# name = 'Erick'
+# print(name[1:-1]) #will print from 1 to -2 (eliminates -1)
 
-# Begin 37:
+# formatted strings
+# first = 'John'  # you want a statement like John [Smith] is a coder
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder' #first way
+# msg = f'{first} [{last}] is a coder' # f for formatted strings
+# print(message)
+# print(msg)
+
+# String Methods --- variable.x
+# .find finds the index, in gives a boolean output
+# course = 'Python for Beginners'
+# print(course.upper())
+# print(course.lower())
+# print(course.find('Z')) #find the index of the character. If -1, character not present.
+# print(course.find('Beginners')) # returns index of B.
+# print(course.replace('Beginners','Absolute Beginners')) #replace
+# print('Python' in course) #in checks if there's a character in the data set. Returns Boolean
+
+# Arithmetic operations +, /, // (integer)
+# print(1000//3) #returns 333 (integer)
+# print(100 % 3) #modulus returns the remainder of the division ie, 1
+# print(10 ** 3) #power of ...
+# x = 10 #add 3 to x
+# # x = x + 3
+# x += 3 #augmented/enhanced assignment interpreter - same as x = x+3
+# # use with -=, *=
+# print(x)
+
+# Operator precedence - BODMAS
+
+# Math Functions - at the top of .py, write import math
+# x = 2.54673
+# print(round(x)) #rounds to a whole number
+# print(abs(-2.9)) #absolute returns a positive value
+# import math  #makes maths functions available
+# from math import floor
+# print(math.ceil(5.1)) #rounds up a number ie, 6
+# print(floor(3.8993)) #rounds down a number ie, 3
+
---
+++ mosh2.py
@@ -0,0 +1,17 @@
+# if statements
+# requirements: if it's hot, It's a hot day + Drink plenty of water
+#               otherwise if it's cold, It's a cold day + Wear warm clothes
+#               otherwise, It's a lovely day
+# is_hot = False
+# is_cold = False #either will be executed if ONLY true
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")  # If not indented, appears with or without True.
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else: #executed if both is_hot and is_cold are false
+#     print(" It's a lovely day")
+# print("Enjoy your day")
+
+#start 04:45 do exercise
