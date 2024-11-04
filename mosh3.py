1bf57db328a50790b2b8b46487da26c930d9c0ef
Erick Gathinji <ericgathinji@gmail.com>
2024-11-04 08:42:09 +0300

    inheritance

--- mosh3.py
+++ mosh3.py
@@ -1,2 +1,29 @@
-#FOR loops
-#start 1:42
+# FOR loops - loop variables
+"""
+for item in 'Python':
+    print(item) #outputs each character in a separate line
+for item in ['Mosh', 'Joe', 'Sam','Kim']: #A list
+    print(item)
+
+# Use a range when dealing with large data set
+for item in range(0,10,2): #use a third number to specify gaps
+    print(item)
+
+# exercise find the total of the price list using for loop
+prices = [10, 20, 30]
+total = 0 # start by defining a variable
+for item in prices:
+    total += item
+print(total) #make sure to indent well outside
+
+# Nested Loops - a loop inside another
+for x in range(4): # other values (1,2,3) will be executed after y. Each value executes the y values
+    for y in range (3):
+        print(f'({x}, {y})')
+"""
+# Challenge
+
+numbers = [5, 2, 5, 2, 2]
+
+for x_count in numbers:
+# start 1:53:00
